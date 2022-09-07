82cdba72586b1c52bbe06521441b53ea05a0f19f
Joy Evans <joyevans@me.com>
2022-09-06 19:42:59 -0500

    modified buttons, footer, & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageLoginComponent } from './landing-page-login/landing-page-login.component';
+import { LandingPageSignupComponent } from './landing-page-signup/landing-page-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing-page-component', component: LandingPageComponent },
+  {path: 'landing-page-login-component', component: LandingPageLoginComponent },
+  {path: 'landing-page-signup-component', component: LandingPageSignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
